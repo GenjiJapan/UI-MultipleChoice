f888aa8acbdea82ec096940ee622c42af4957c6f
Le <nghi.le@bigdutchman.com>
2021-05-24 10:06:02 +0700

    second commit

---
+++ Modal.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById("myModal");
+modal.style.display = "none";
+
+const showModal = () => {
+    modal.style.display = "block";
+}
+
+const closeModal = () => {
+    modal.style.display = "none";
+}
